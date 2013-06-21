b816d6f5596c110080412f8adb1f7218655655fa
Christopher Hopkins <chris@chocol.it>
2013-06-21 07:22:18 -0400

    add chart.js

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A personal statistics dashboard and API.
 ## Inspiration
 
 I wrote about this a little while back [Personal API • Quantified Self](http://blog.chocol.it/2013/06/08/personal-api/).
+
+## Credit
+
+- [Chart.js](https://github.com/nnnick/Chart.js) — Simple HTML5 Charts using the <canvas> tag.
---
+++ app/assets/javascripts/data.js
@@ -0,0 +1,23 @@
+var doughnutData = [
+    {
+      value: 30,
+      color:"#F7464A"
+    },
+    {
+      value : 50,
+      color : "#46BFBD"
+    },
+    {
+      value : 100,
+      color : "#FDB45C"
+    },
+    {
+      value : 40,
+      color : "#949FB1"
+    },
+    {
+      value : 120,
+      color : "#4D5360"
+    }
+
+  ];
