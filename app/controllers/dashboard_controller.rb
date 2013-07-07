fcc3a5ee129ee4b58c6837bdcff40badc16aaf7a
Christopher Hopkins <chris@chocol.it>
2013-07-06 20:03:16 -0400

    wire up mood in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -27,6 +27,33 @@ class DashboardController < ApplicationController
     if Mood.last.present?
       @mood_title = Mood.title
       @mood_sub_type  = Mood.sub_type
+
+      case @mood_sub_type
+      when 1
+        @mood_face  = "Amazing!"
+        @mood_scale = 8
+      when 2
+        @mood_face  = "Pumped UP"
+        @mood_scale = 7
+      when 3
+        @mood_face  = "Energized"
+        @mood_scale = 6
+      when 4
+        @mood_face  = "Good"
+        @mood_scale = 5
+      when 5
+        @mood_face  = "Meh"
+        @mood_scale = 4
+      when 6
+        @mood_face  = "Dragging"
+        @mood_scale = 3
+      when 7
+        @mood_face  = "Exhausted"
+        @mood_scale = 2
+      when 8
+        @mood_face  = "Totally Done"
+        @mood_scale = 1
+      end
     end
   end
 end
