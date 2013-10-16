8a80ebb5bb25f61c6fe4ba6238747fa029e24077
Christopher Hopkins <chris@chocol.it>
2013-10-15 21:50:50 -0400

    reimplement mood adjustment view helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,33 @@
 module DashboardHelper
+  def mood_adjustment(mood)
+    case mood.sub_type
+    when 0
+      mood.title = "No Data"
+      return 8
+    when 1
+      mood.title = "Amazing!"
+      return 8
+    when 2
+      mood.title = "Pumped UP"
+      return 7
+    when 3
+      mood.title = "Energized"
+      return 6
+    when 4
+      mood.title = "Meh"
+      return 4
+    when 5
+      mood.title = "Dragging"
+      return 3
+    when 6
+      mood.title = "Exhausted"
+      return 2
+    when 7
+      mood.title = "Totally Done"
+      return 1
+    when 8
+      mood.title = "Good"
+      return 5
+    end
+  end
 end
