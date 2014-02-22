df87da29b7e69c52020a82793d78e5752907d829
Christopher Hopkins <chris@hopkins.io>
2014-02-22 12:46:07 -0500

    route type

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Dashboard::Application.routes.draw do
   root "dashboard#index"
 
-  namespace :api, path: "", contraints: { subdomain: "api" } do
+  namespace :api, path: "", constraints: { subdomain: "api" } do
     namespace :v0 do
       resources :users
       resources :sleeps
